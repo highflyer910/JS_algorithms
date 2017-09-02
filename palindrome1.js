85f6da05e67b5cdea91ea5e7d323b2454bbfc4d2
highflyer910 <tmush2014@agruni.edu.ge>
2017-09-02 21:06:49 +0400

    palindrome1

---
+++ palindrome1.js
@@ -0,0 +1,12 @@
+function isPalindrome(string) {
+  string = string.toLowerCase();
+  var charactersArr = string.split('');
+  var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  
+  var lettersArr = [];
+  charactersArr.forEach(char => {
+    if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+  });
+  
+  return lettersArr.join('') === lettersArr.reverse().join('');
+}
