e7db8f9354348ae535c90d00f03018d18c866908
highflyer910 <tmush2014@agruni.edu.ge>
2017-09-06 18:00:19 +0400

    Reverse a String With a Decrementing For Loop

---
+++ reversestring.js
@@ -0,0 +1,10 @@
+//Reverse a String With a Decrementing For Loop
+
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
