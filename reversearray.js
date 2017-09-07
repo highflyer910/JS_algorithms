0e52762bac4aaf5f89a3e834ee8f3599e47a85f7
highflyer910 <tmush2014@agruni.edu.ge>
2017-09-07 19:17:26 +0400

    Reverse Array

---
+++ reversearray.js
@@ -0,0 +1,9 @@
+function reverseArray(arr) {
+  for (var i = 0; i < arr.length / 2; i++) {
+    var temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+  
+  return arr;
+}
