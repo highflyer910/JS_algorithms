fa8a209b5a6f16aa43903bc3458e1baceb175f9f
highflyer910 <tmush2014@agruni.edu.ge>
2017-09-11 17:34:23 +0400

    Sum Array

---
+++ twosum.js
@@ -0,0 +1,15 @@
+function twoSum(numArray, sum) {
+  var pairs = [];
+  var hashTable = [];
+ 
+  for (var i = 0; i < numArray.length; i++) {
+    var currNum = numArray[i];
+    var counterpart = sum - currNum;
+    if (hashTable.indexOf(counterpart) !== -1) {
+      pairs.push([ currNum, counterpart ]);
+    }
+    hashTable.push(currNum);
+  }
+  
+  return pairs;
+}
