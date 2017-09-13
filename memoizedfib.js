35caf645a07e40d33c2726b9a6d6d770cd91a092
highflyer910 <tmush2014@agruni.edu.ge>
2017-09-13 17:13:15 +0400

    Memoized Fibonacci

---
+++ memoizedfib.js
@@ -0,0 +1,14 @@
+function fibMemo(index, cache){
+     cache = cache || [];
+     if(cache[index]){
+     	return cache[index];
+     } else {
+     	if(index < 3){
+     		return 1;
+     	} else {
+     		cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+     	}
+     }
+
+     return cache[index];
+}
