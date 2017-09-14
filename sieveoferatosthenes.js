0d2d9f3503b5ed6be75e08c8a9d4d010f9d0ab19
highflyer910 <tmush2014@agruni.edu.ge>
2017-09-14 15:26:23 +0400

    Sieve of Erastothenes

---
+++ sieveoferatosthenes.js
@@ -0,0 +1,22 @@
+function sieveOfEratosthenes(n) {
+  var primes = [];
+  for (var i = 0; i <= n; i++) {
+    primes[i] = true;
+  }
+  
+  primes[0] = false;
+  primes[1] = false;
+  
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    for (j = 2; i * j <= n; j++) {
+      primes[i * j] = false;
+    }
+  }
+  
+  var result = [];
+  for (var i = 0; i < primes.length; i++) {
+    if (primes[i]) result.push(i);
+  }
+  
+  return result;
+}
