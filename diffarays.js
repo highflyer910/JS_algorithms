a8b463d2d2f7082a04bc15a1e3c8b9e2b2eb5050
highflyer910 <tmush2014@agruni.edu.ge>
2017-09-19 22:09:43 +0400

    Diff two arrays

---
+++ diffarays.js
@@ -0,0 +1,11 @@
+function diff(arr1, arr2) {
+  var newArr = arr1.concat(arr2);
+
+  function check(item) {
+    if (arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1) {
+      return item;
+    }
+  }
+
+  return newArr.filter(check);
+}
