7073704b4c9581c639296920a26fba60b6d45f7c
Thea <tmush2014@agruni.edu.ge>
2018-03-25 18:00:21 +0400

    C to F

--- convertof.js
+++ convertof.js
@@ -1,10 +1,4 @@
 function convertToF(celsius) {
-  var fahrenheit = (celsius * (9 / 5)) + 32;
-   if ( typeof fahrenheit !== 'undefined' ) {
+  let fahrenheit = (celsius * (9/5)) + 32;
   return fahrenheit;
-  } else {
-    return 'fahrenheit not defined';
-  }
 }
-
-convertToF(30);
