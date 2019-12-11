e30ead3ef322163cc532bc096dbd9863c6b7177c
Thea <thea.mushambadze@gmail.com>
2019-12-12 00:02:45 +0400

    fizz buzz

---
+++ find-avg.js
@@ -0,0 +1,11 @@
+function findAvg(arr){
+	var avg;
+	var sum = 0;
+	arr.foreach(function(elem){
+	   sum += elem;
+	});
+	avg = sum/arr.length;
+
+	return avg;
+
+}
--- fizz-buzz.js
+++ fizz-buzz.js
@@ -6,7 +6,7 @@
 
 function fizzBuzz(num){
 	for(var i = 0; i <= num; i++){
-		if(i % 15 === 0){
+		if(i % 3 === 0 && i % 5 === 0){
 			console.log('FizzBuzz');
 		} else if (i % 3 === 0){
             console.log('Fizz');
