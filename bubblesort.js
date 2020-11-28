8cf412eef940e677e921f0958d985e5fd4f72789
Thea <thea.mushambadze@gmail.com>
2020-11-29 01:19:28 +0400

    bubblsort.js

--- bubblesort.js
+++ bubblesort.js
@@ -1,9 +1,13 @@
+// To sort an array of integers from lowest to highest value.
 function bubbleSort(array) {
     for (var i = array.length; i > 0; i--) {
+      // Notice that j < (length - i)
       for (var j = 0; j < i; j++) {
+        // Notice that j < (length - i)
         if (array[j] > array[j + 1]) {
-          var temp = array[j];
-          array[j] = array[j + 1];
+          // Swap the numbers
+          var temp = array[j]; // Temporary variable to hold the current number
+          array[j] = array[j + 1]; // Replace current number with adjacent number
           array[j + 1] = temp;
         }
       }
